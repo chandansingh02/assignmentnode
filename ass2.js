4c99ca90bb234382074d39f81fab15b0ed39a813
chandansingh02 <chan02nov@gmail.com>
2014-08-25 22:48:15 +0530

    commited

--- ass2.js
+++ ass2.js
@@ -1,10 +1,25 @@
 var fs = require ('fs');
-var randomword = require ('random-words');
-fs.appendFile('hello1.txt',"randomword()+='\n'", function(print)
-{if(print) {
-	console.log(print);
-}
-	else{
-	console.log("the content is save");
-	}
+var randomword = require ("random-words");
+fs.appendFile('hello1.txt',randomword()+'\n', function(err)  
+
+{ 
+	if (err) {
+		
+    console.log(err);
+	          }
+	else { 
+		fs.readFile('hello1.txt','utf-8',function (err,data) {
+    if (err)  {
+    return console.log(err);
+          		}
+          else {
+          	console.log(data);
+          	var lines = data.toString().split('\n').length - 1;
+                console.log(lines);
+          	} 
+        });  
+      
+         
+        console.log("Random word successfully written to the file.");
+    }
 });
